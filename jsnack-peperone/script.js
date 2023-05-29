2f803c92145303fa83f61194aec4d9337cd72438
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-29 12:28:22 +0200

    jsnack-peperone

--- jsnack-peperone/script.js
+++ jsnack-peperone/script.js
@@ -3,14 +3,24 @@ const list = [];
 function pepperObject(){
     const pepper = {
         type : '',
-        weight : (Math.random() * 3 + 1).toFixed(2) + 'kg',
-        length : (Math.random() * 70 + 1).toFixed(2) + 'cm',
+        weight : (Math.random() * 3 + 1).toFixed(2),
+        length : (Math.random() * 70 + 1).toFixed(2),
     }
     return pepper;
 }
 
 for(let i = 0; i < 10; i++){
     list.push(pepperObject());
+    
 }
 
 console.log(list);
+let totalWeight= 0;
+for(let a = 0; a < list.length; a++){
+    const pepper = list[a];
+
+    totalWeight += pepper.weight;
+
+}
+
+console.log(totalWeight);
